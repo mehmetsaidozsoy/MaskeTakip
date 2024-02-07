d70ba0a4fa09d347821891443c0deefff4d76a30
mehmetsaidozsoy <mehmetsaidozsoy@gmail.com>
2024-02-07 04:58:19 +0300

    2024 Yazılım Geliştirici Yetiştirme Kampı (C#) Ödev 2 son commit

--- Business/Concrete/PersonManager.cs
+++ Business/Concrete/PersonManager.cs
@@ -1,16 +1,26 @@
-﻿using Entities.Concrete;
+﻿using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference;
 
 namespace Business.Concrete
 {
-    public class PersonManager
+    public class PersonManager:IApplicantService
     {
         public void ApplyForMask(Person person)
         {
 
         }
-        public List<Person>? GetList() 
+        public List<Person>? GetList()
         {
             return null;
+        }
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client=new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+
+            return client.TCKimlikNoDogrulaAsync
+                (new TCKimlikNoDogrulaRequest
+                (new TCKimlikNoDogrulaRequestBody(person.NationalIdentity,person.FirstName,person.LastName,person.DateOfBirthYear))).Result.Body.TCKimlikNoDogrulaResult;
         }
     }
 }
--- WorkAround/Program.cs
+++ WorkAround/Program.cs
@@ -1,12 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Vatandas vatandas = new Vatandas();
+using Business.Concrete;
+using Entities.Concrete;
 
-public class Vatandas
-{
-    public string Ad { get; set; }
-    public string Soyad { get; set; }
-    public int Dogumyili { get; set; }
-    public long TcNo { get; set; }
+PttManager pttManager = new PttManager(new PersonManager());
 
-
-}
+//Vatandas vatandas = new Vatandas();
+Person person1 = new Person();
+person1.FirstName = "Mehmet Said";
+person1.LastName = "Özsoy";
+person1.DateOfBirthYear = 1980;
+person1.NationalIdentity = 30935387884;
+pttManager.GiveMask(person1);
